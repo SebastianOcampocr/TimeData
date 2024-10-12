33c566c866d1091831e396c0e7dbfc022dab07dc
Sebassssssssssss <81488575+SebastianOcampocr@users.noreply.github.com>
2024-10-11 22:01:17 -0600

    Update IniciarSesion.jsx

--- src/IniciarSesion.jsx
+++ src/IniciarSesion.jsx
@@ -18,7 +18,7 @@ const IniciarSecion = () => {
         }
     
         try {
-            const response = await fetch('http://localhost:5000/login', {
+            const response = await fetch(https://timedata.azurewebsites.net/login, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
