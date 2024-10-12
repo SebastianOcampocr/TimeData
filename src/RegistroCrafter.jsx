24d29704e3212f6e21e6e310f80a26e419d70e25
Sebassssssssssss <81488575+SebastianOcampocr@users.noreply.github.com>
2024-10-11 18:10:28 -0600

    Update RegistroCrafter.jsx

--- src/RegistroCrafter.jsx
+++ src/RegistroCrafter.jsx
@@ -110,7 +110,8 @@ const Registrocrafter = () => {
                     Registrarse
                 </button>
                 <div className="ayudaaa">
-                    <a href="#" className="help-link">¿Necesitas ayuda?</a>
+                    <a href="/help" className="help-link">¿Necesitas ayuda?</a>
+
                 </div>
             </div>
             <div className="ya-tienes-una-container">
